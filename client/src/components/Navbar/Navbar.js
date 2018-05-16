687292001d9339ff458182e7805058cf4445f478
Travo100 <travo100@gmail.com>
2018-05-15 17:56:50 -0700

    commit

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 const Navbar = () => (
-    <nav className="navbar navbar-inverse">
-        <div className="container-fluid">
-            <div className="navbar-header">
-                <Link className="navbar-brand" to="/">Chihuahua Clicker</Link>
-            </div>
-            <ul className="nav navbar-nav navbar-right">
-                <Link className="navbar-brand" to="/">Home</Link>
-                <Link className="navbar-brand" to="/submit">Submit</Link>
-            </ul>
-        </div>
-    </nav>
+  <nav className="navbar navbar-inverse">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <Link className="navbar-brand" to="/">Chihuahua Clicker</Link>
+      </div>
+      <ul className="nav navbar-nav navbar-right">
+        <Link className="navbar-brand" to="/">Home</Link>
+        <Link className="navbar-brand" to="/submit">Submit</Link>
+      </ul>
+    </div>
+  </nav>
 );
 
 export default Navbar;
